94f578b86e6fb79e3cebe3309202d40d7b1fd11c
Pascal Lauer <pascal.lauer@outlook.de>
2026-01-27 09:28:45 +0100

    first landing page design

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Hier kannst du dein JavaScript Code schreiben
-
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Seite geladen');
+
+    // --- BUTTON 1: Start Exploring ---
+    const startBtn = document.getElementById("startBtn");
+    startBtn.addEventListener("click", start_exploring);
+
+    // --- BUTTON 2: Learn More ---
+    const learnBtn = document.getElementById("learnBtn");
+    learnBtn.addEventListener("click", learn_more);
 });
+
+// Die Funktionen können außerhalb stehen bleiben
+function start_exploring() {
+    // 1. Das Ziel suchen
+    const ziel = document.querySelector(".orte-bereich");
+    
+    // 2. Weich dorthin scrollen
+    ziel.scrollIntoView({ behavior: "smooth" });
+}
+
+function learn_more() {
+    alert("Learn More wurde geklickt!");
+}
